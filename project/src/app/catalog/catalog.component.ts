ed666536fb10073602811a77de4aed6282fceeb3
Mhmd0616 <158848109+Mhmd0616@users.noreply.github.com>
2024-11-03 03:24:02 +0200

    .

--- project/src/app/catalog/catalog.component.ts
+++ project/src/app/catalog/catalog.component.ts
@@ -43,6 +43,7 @@ export class CatalogComponent implements OnInit {
     if (userEmail) {
       this.cartService.addToCart(product.id, product.selectedQuantity, userEmail).subscribe(response => {
         console.log('Product added to cart:', response.message);
+        alert('Product added to cart');
       }, error => {
         console.error('Error adding product to cart:', error);
       });
