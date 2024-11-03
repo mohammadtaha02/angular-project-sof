06a25354736ca2e03578c377b28b6b84111da7a5
mohammadtaha02 <m.taha.coursera@gmail.com>
2024-11-03 20:20:51 +0200

    a

--- project/src/app/app.module.ts
+++ project/src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule, Routes } from '@angular/router';
@@ -92,6 +93,7 @@ const routes: Routes = [
 @NgModule({
   declarations: [
     AppComponent,
+    PlanComponent,
     NavbarComponent,
     FooterComponent,
     AboutComponent,
@@ -115,7 +117,11 @@ const routes: Routes = [
     WorkoutDetailsComponent,
     CartComponent,
     ContactUsComponent,
+<<<<<<< Updated upstream
     PlanComponent,
+=======
+    ActualWorkoutsComponent
+>>>>>>> Stashed changes
   ],
   imports: [
     BrowserModule,
